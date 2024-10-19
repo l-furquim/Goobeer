7bc3e659dddf6457bfa422d1d0a562a45e8a8717
l-furquim <lucas.furquim@sptech.school>
2024-10-19 14:05:03 -0300

    Change footer style and icons

--- front/src/app/dashboard/_components/footer.tsx
+++ front/src/app/dashboard/_components/footer.tsx
@@ -1,36 +1,36 @@
 import { ThemeButton } from "@/components/theme/theme-button";
-import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
-import { ShoppingCartIcon } from "lucide-react";
+import { TwitterLogoIcon } from "@radix-ui/react-icons";
+import { GithubIcon, ShoppingCartIcon, TwitterIcon } from "lucide-react";
 import Link from "next/link";
 
 const Footer = () => {
 
     return (
         <div className="text-zinc-900 font-bold bg-zinc-300 h-32 flex flex-row items-center gap-8 mt-8 ">
-            <div className="ml-10 flex flex-row gap-10">
+            <div className="ml-10 w-full flex flex-row gap-10">
                 <div className="flex flex-row gap-10">
                     <ShoppingCartIcon size={"42px"}/>
                     <span className="mt-3">© 2024  </span>
                     <span className="mt-3">Goober e-commerce</span>
                 </div>
             
-                <div className="absolute end-48 mt-3">
+                <div className="flex ml-auto mr-14 gap-4 mt-3">
                     <Link href="#">
                         Sobre nós
                     </Link>
-                </div>
-                <div className="mt-5 absolute end-32">
+                
+                <div className=" rounded-full border p-1  border-black">
                     <Link href="https://github.com/l-furquim/Goober">
-                            <GitHubLogoIcon/>
+                            <GithubIcon size={22}/>
                     </Link>
                 </div>
-                <div className="mt-5 absolute end-20">
+                
+                <div className=" rounded-full border p-1  border-black">
                     <Link href="#">
-                        <TwitterLogoIcon/>
+                        <TwitterIcon size={22}/>
                     </Link>
                 </div>
-                <div>
-                    <ThemeButton/>
+
                 </div>
             </div>
         </div>
